dd0f8256c2d461c57e2115083f2b77e69cdff81f
laysan <laysansh@gmail.com>
2023-11-28 18:16:27 -0700

    playwright

--- src/test/java/TMSeleniumTest.java
+++ src/test/java/TMSeleniumTest.java
@@ -1,8 +1,12 @@
+import dev.failsafe.internal.util.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -10,27 +14,13 @@ import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 
-public class TMobileSelenium {
+public class TMSeleniumTest {
 
     static WebDriver driver = new ChromeDriver();
     static WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-
     private final static String ALL_SUB_FILTERS = "//div[@role='group']//span[@class='filter-display-name']";
 
-    public static void main(String[] args) throws InterruptedException {
-
-        driver.get("https://www.t-mobile.com/tablets");
-        Thread.sleep(5000);
-
-       selectFilter("Brands", "Apple", "Samsung", "TCL");
-//
-//        selectFilter("Brands", "TCL");
-        selectFilter("Deals", "New", "Special offer");
-        selectFilter("Operating System", "iPadOS", "Android");
-//        selectFilter("Brands", "All");
-    }
-
-    static void selectFilter(String filter, String... subFilters) {
+    void selectFilter(String filter, String... subFilters) {
 
         WebElement filterElement = wait.until(ExpectedConditions.elementToBeClickable(
                 By.xpath("//legend[normalize-space() = '" + filter + "']")));
@@ -42,7 +32,7 @@ public class TMobileSelenium {
 
         if (subFilters.length == 1 && subFilters[0].equals("All")) {
             for (WebElement element : actualSubFilters) {
-               element.click();
+                element.click();
             }
         } else if (subFilters.length > 0 && !Arrays.toString(subFilters).contains("All"))  {
 
@@ -52,5 +42,25 @@ public class TMobileSelenium {
                 filterDisplayName.click();
             }
         }
+    }
+
+    @Test
+    public void testSelectOption1() throws InterruptedException {
+
+        List<String> expectedSubFilters = List.of("Apple", "Samsung", "TCL");
+
+        driver.get("https://www.t-mobile.com/tablets");
+
+        Thread.sleep(5000);
+
+        selectFilter("Brands", expectedSubFilters.get(0), expectedSubFilters.get(1), expectedSubFilters.get(2));
+
+        List<String> checkedSubFilters = driver.findElements(
+                By.xpath(
+         "//label[@class='mat-checkbox-layout']//input[@aria-checked='true']/parent::span/following-sibling::span/span[@class='filter-display-name']"))
+                .stream()
+                .map(WebElement::getText).toList();
+
+        Assertions.assertEquals(expectedSubFilters, checkedSubFilters);
     }
 }
